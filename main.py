2c1b61c074d8b2688d4acdd0ce847babead84a99
Susanna-Salata <susanna.kiev@gmail.com>
2022-02-11 20:50:35 +0100

    done

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 from datetime import datetime
 import datetime as dt
 
+WEEKDAYS = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
+
+
 def helper(date_val):
     return dt.date(datetime.today().year, date_val.month, date_val.day)
 
+
 def next_week(date_val):
     start = datetime.today().date()
-    end = datetime.today().date() + dt.timedelta(days=7)
+
+    if datetime.weekday(start) == 5:
+        end = datetime.today().date() + dt.timedelta(days=6)
+    elif datetime.weekday(start) == 6:
+        end = datetime.today().date() + dt.timedelta(days=5)
+    else:
+        end = datetime.today().date() + dt.timedelta(days=7)
     return (date_val >= start) and (date_val <= end)
 
 
+def shift(date_val):
+    weekday = WEEKDAYS[datetime.weekday(helper(date_val))]
+    if weekday in ["Saturday", "Sunday"]:
+        weekday = "Monday"
+    return weekday
+
+
 def get_birthdays_per_week(users):
-    weekdays = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
-    birthdays = {u["name"]:weekdays[datetime.weekday(helper(u["birthday"]))]
-                 for u in users
-                 if next_week(helper(u["birthday"]))}
-
-    print(birthdays)
-
+    birthdays = {u["name"]:shift(u["birthday"]) for u in users if next_week(helper(u["birthday"]))}
+    #print(birthdays)
+    for day in WEEKDAYS.values():
+        user_names = ", ".join([u for u, d in birthdays.items() if d == day])
+        if user_names:
+            print(f"{day}: {user_names}")
 
 
 if __name__ == '__main__':
-    users = [{"name":"Ivan", "birthday": dt.date(1980, 2, 9)},
-             {"name":"Roman", "birthday": dt.date(1980, 2, 6)},
-             {"name":"Sahsa", "birthday": dt.date(1980, 2, 5)},
-             {"name":"Masha", "birthday": dt.date(1980, 2, 8)},
-             {"name":"Dasha", "birthday": dt.date(1980, 3, 8)},
-             {"name":"Pasha", "birthday": dt.date(1980, 1, 8)}]
-
+    users = [{"name":"Ivan", "birthday": datetime.today().date() + dt.timedelta(days=6)},
+             {"name":"Roman", "birthday": datetime.today().date() + dt.timedelta(days=16)},
+             {"name":"Sahsa", "birthday": datetime.today().date() + dt.timedelta(days=2)},
+             {"name":"Masha", "birthday": datetime.today().date() + dt.timedelta(days=3)},
+             {"name":"Dasha", "birthday": datetime.today().date() + dt.timedelta(days=8)},
+             {"name":"Pasha", "birthday": datetime.today().date() + dt.timedelta(days=-6)}]
+    print(users)
     get_birthdays_per_week(users)
